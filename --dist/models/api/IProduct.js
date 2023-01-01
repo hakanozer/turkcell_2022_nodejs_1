b4094f961e47ec67326a53f3369dc02b2dcf4d12
Hakan ÖZER <hakanozer02@gmail.com>
2023-01-01 12:42:28 +0300

    update

--- --dist/models/api/IProduct.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SaleType = void 0;
-var SaleType;
-(function (SaleType) {
-    SaleType["Sat\u0131l\u0131k"] = "Sat\u0131l\u0131k";
-})(SaleType = exports.SaleType || (exports.SaleType = {}));
-//# sourceMappingURL=IProduct.js.map
